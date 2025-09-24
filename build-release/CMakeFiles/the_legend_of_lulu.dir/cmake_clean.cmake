baede57631dc02dcb446600cf1737a00ca1eb5e2
Pietro Zaccarini <pietrozaccarini6@gmail.com>
2025-09-24 12:39:03 +0200

    On branch main  Your branch is ahead of 'origin/main' by 2 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   .vscode/tasks.json
    	new file:   assets/dungeon/configs/zelda's room.json
    	new file:   assets/dungeon/zelda's lullaby.mp3
    	new file:   assets/dungeon/zelda's room.png
    	new file:   build-release/CMakeCache.txt
    	new file:   build-release/CMakeFiles/3.31.6/CMakeCXXCompiler.cmake
    	new file:   build-release/CMakeFiles/3.31.6/CMakeDetermineCompilerABI_CXX.bin
    	new file:   build-release/CMakeFiles/3.31.6/CMakeSystem.cmake
    	new file:   build-release/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp
    	new file:   build-release/CMakeFiles/CMakeConfigureLog.yaml
    	new file:   build-release/CMakeFiles/CMakeDirectoryInformation.cmake
    	new file:   build-release/CMakeFiles/Makefile.cmake
    	new file:   build-release/CMakeFiles/Makefile2
    	new file:   build-release/CMakeFiles/TargetDirectories.txt
    	new file:   build-release/CMakeFiles/cmake.check_cache
    	new file:   build-release/CMakeFiles/progress.marks
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/DependInfo.cmake
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/build.make
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/cmake_clean.cmake
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/compiler_depend.internal
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/compiler_depend.make
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/compiler_depend.ts
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/depend.make
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/flags.make
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/link.txt
    	new file:   build-release/CMakeFiles/the_legend_of_lulu.dir/progress.make
    	new file:   build-release/Makefile
    	new file:   build-release/cmake_install.cmake
    	new file:   build-release/the_legend_of_lulu
    	new file:   build/.cmake/api/v1/query/client-vscode/query.json
    	new file:   build/.cmake/api/v1/reply/cache-v2-a9594e1b8d02aefc1699.json
    	new file:   build/.cmake/api/v1/reply/cmakeFiles-v1-c89d8a88e0ad6d789d38.json
    	new file:   build/.cmake/api/v1/reply/codemodel-v2-1cb44a7c127f78a6dade.json
    	new file:   build/.cmake/api/v1/reply/directory-.-Debug-f5ebdc15457944623624.json
    	new file:   build/.cmake/api/v1/reply/index-2025-09-24T10-38-37-0185.json
    	new file:   build/.cmake/api/v1/reply/target-the_legend_of_lulu-Debug-0c6e5de61a7ab814bfd6.json
    	new file:   build/.cmake/api/v1/reply/toolchains-v1-de874c9bdb2ffa785473.json
    	new file:   build/CMakeCache.txt
    	new file:   build/CMakeFiles/3.31.6/CMakeCXXCompiler.cmake
    	new file:   build/CMakeFiles/3.31.6/CMakeDetermineCompilerABI_CXX.bin
    	new file:   build/CMakeFiles/3.31.6/CMakeSystem.cmake
    	new file:   build/CMakeFiles/3.31.6/CompilerIdCXX/CMakeCXXCompilerId.cpp
    	new file:   build/CMakeFiles/CMakeConfigureLog.yaml
    	new file:   build/CMakeFiles/TargetDirectories.txt
    	new file:   build/CMakeFiles/cmake.check_cache
    	new file:   build/CMakeFiles/rules.ninja
    	new file:   build/build.ninja
    	new file:   build/cmake_install.cmake
    	new file:   build/compile_commands.json

---
+++ build-release/CMakeFiles/the_legend_of_lulu.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/the_legend_of_lulu.dir/link.d"
+  "CMakeFiles/the_legend_of_lulu.dir/game/main.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/game/main.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/game/src/game.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/game/src/game.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/game/src/gameScene.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/game/src/gameScene.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/game/src/gameplay.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/game/src/gameplay.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/game/src/menu.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/game/src/menu.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/actor.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/actor.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/animationHandler.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/animationHandler.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/arena.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/arena.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/fighters/fighter.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/fighters/fighter.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/fighters/link.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/fighters/link.cpp.o.d"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/movable.cpp.o"
+  "CMakeFiles/the_legend_of_lulu.dir/lulu/src/movable.cpp.o.d"
+  "the_legend_of_lulu"
+  "the_legend_of_lulu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/the_legend_of_lulu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
